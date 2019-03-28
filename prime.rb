0278d54e7fc271950b1aa1719e5ecd3f0f821c86
Jennifer Busca <jennifer.busca@gmail.com>
2019-03-28 12:22:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 def prime?(number)
   (1...number).each do |i|
     if number % i == 0
-      return 
-        false
-      end
-    return true
+      return false
+    end
+  end
+  return true
 end
